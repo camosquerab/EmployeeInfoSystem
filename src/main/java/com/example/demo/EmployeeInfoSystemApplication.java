f55467ade95dd1dfb67d2c09a12207f05db32203
Camilo Mosquera <camosquerab@unal.edu.co>
2024-08-16 16:56:38 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/EmployeeInfoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInfoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInfoSystemApplication.class, args);
+	}
+
+}
